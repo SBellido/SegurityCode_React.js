a4d63192c478aacd5917ff56ff05aa63cfd4f0fb
SBellido <sebastianbellidodg@gmail.com>
2024-06-05 12:55:10 -0300

    useState was implemented in functions and Classes

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,13 +1,36 @@
 import React from "react";
 
+/* Manejo básico del Estado en Clases utilizando 
+React Hooks, useState de forma individual y declarativa */
 class ClassState extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: false,
+    };
+  }
+
   render() {
     return (
       <div>
-        <h2>Elminar ClassState</h2>
+        <h2>Elminar {this.props.name}</h2>
+
         <p>Por favor, escribe el código de seguridad</p>
+
+        {this.state.error && <p>Error: el código es incorrecto.</p>}
+
         <input placeholder="Código de seguridad" />
-        <button>Comprobar</button>
+        <button
+          onClick={() =>
+            this.setState((prevState) => ({ error: !prevState.error }))
+          }
+        >
+          Comprobar
+        </button>
+        {/* <button onClick={() => this.setState({ error: !this.state.error })}>
+          Comprobar
+        </button> */}
       </div>
     );
   }
--- src/UseState.js
+++ src/UseState.js
@@ -1,12 +1,19 @@
 import React from "react";
 
-function UseState() {
+/* Manejo básico del Estado en funciones utilizando 
+React Hooks, useState de forma individual y declarativa */
+function UseState({ name }) {
+  const [error, setError] = React.useState(false);
+
   return (
     <div>
-      <h2>Elminar UseState</h2>
+      <h2>Elminar {name}</h2>
       <p>Por favor, escribe el código de seguridad</p>
+
+      {error && <p>Error: el código es incorrecto.</p>}
+
       <input placeholder="Código de seguridad" />
-      <button>Comprobar</button>
+      <button onClick={() => setError(!error)}>Comprobar</button>
     </div>
   );
 }
